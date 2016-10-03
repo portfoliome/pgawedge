a617c4bd3b38b2d364a3f12a0cb066559f8c8ae3
pmart123 <philip.martin2007@gmail.com>
2016-10-03 13:32:50 -0400

    Add specification for alchemy schema.

---
+++ tests/test_alchemy.py
@@ -0,0 +1,24 @@
+import unittest
+
+import sqlalchemy as sa
+
+from pga.alchemy import sa_column_dict_to_column
+
+
+class TestAlchemySchema(unittest.TestCase):
+
+    def test_sa_column_dict_to_column(self):
+        column_name = 'my_mock_column'
+        column_type = sa.CHAR(length=2)
+        nullable = False
+
+        attributes = {
+            'autoincrement': False, 'default': None, 'name': column_name,
+            'nullable': nullable, 'type': column_type
+        }
+
+        result_column = sa_column_dict_to_column(attributes)
+        result = {attribute: getattr(result_column, attribute)
+                  for attribute in attributes.keys()}
+
+        self.assertEqual(attributes, result)
