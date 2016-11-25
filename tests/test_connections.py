fa8edeb3dddacfa8a2203bf521d630108576468e
pmart123 <philip.martin2007@gmail.com>
2016-11-24 21:52:02 -0500

    Specification for alchemy postgres url, and forking connection from pool.

---
+++ tests/test_connections.py
@@ -0,0 +1,25 @@
+import unittest
+
+from psycopg2.extensions import connection as pg_connection
+
+from pgawedge.connections import format_alchemy_url, get_raw_connection
+
+from .alchemy_fixtures import AlchemySQLFixture
+
+
+class TestSqlalchemyConnections(AlchemySQLFixture, unittest.TestCase):
+    def test_format_alchemy_url(self):
+        host = 'myhost'
+        database = 'mydb'
+        user = 'myuser'
+        password = 'mypassword'
+
+        expected = 'postgresql://myuser:mypassword@myhost/mydb'
+        result = str(format_alchemy_url(host, database, user, password))
+
+        self.assertEqual(expected, result)
+
+    def test_get_raw_connection(self):
+        result = get_raw_connection(self.engine)
+
+        self.assertIsInstance(result, pg_connection)
